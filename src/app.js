d0c81a86905b42f9ed988396b849a417eda36767
Alemoretti <alemoretti10@gmail.com>
2024-11-14 06:02:40 -0300

    Create crud requests

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 
 const app = express()
+app.use(express.json())
 
 const books = [
   {
@@ -13,12 +14,38 @@ const books = [
   }
 ]
 
+function getBookById(id) {
+  return books.findIndex(book => book.id === parseInt(id))
+}
+
 app.get("/", (req, res) => {
   res.status(200).send("Node.js Course 2")
 })
 
 app.get("/books", (req, res) => {
   res.status(200).json(books)
+})
+
+app.get("/books/:id", (req, res) => {
+  const index = getBookById(req.params.id)
+  res.status(200).json(books[index])
+})
+
+app.post("/books", (req, res) => {
+  books.push(req.body)
+  res.status(201).send("Book added successfully")
+})
+
+app.put("/books/:id", (req, res) => {
+  const index = getBookById(req.params.id)
+  books[index].title = req.body.title
+  res.status(200).json(books)
+})
+
+app.delete("/books/:id", (req, res) => {
+  const index = getBookById(req.params.id)
+  books.splice(index, 1)
+  res.status(200).send("Book deleted successfully")
 })
 
 export default app
